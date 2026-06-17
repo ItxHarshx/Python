fc193f5dce0d7a01433107d48250dde01d36bc40
ItxHarshx <itxharshx@gmail.com>
2026-06-17 23:55:30 +0530

    Update print.py

--- Print & Comments/print.py
+++ Print & Comments/print.py
@@ -1,5 +1,7 @@
 # print 
-print("Hello World")
+print("Hello World") # printing Hello World
+
+#-----------
 print(12)
 print(2.1)
 print("Gud", 98)
@@ -8,3 +10,8 @@ print(3+2)
 print(7-3)
 print(4*2)
 print(34/2)
+#------------
+
+print("Hello!\nWorld") # escape sequence character: \n
+
+
